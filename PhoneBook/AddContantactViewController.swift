1a6e731bd9c0ce9bcd41777fbb52821a64620b2d
Ilya Gushchin <Ilya.Gushchin@swtecnn.com>
2021-03-19 10:13:36 +0300

    add second controller

---
+++ PhoneBook/AddContantactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddContantactViewController.swift
+//  PhoneBook
+//
+//  Created by Gushchin Ilya on 19.03.2021.
+//
+
+import UIKit
+
+class AddContantactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add contact"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PhoneBook/ViewController.swift
+++ PhoneBook/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView:  UITableView!
+    
+    
+    var footballPlayers = ["Muller",
+                           "Messi",
+                           "Alaba",
+                           "Ilsave",
+                           "Natural",
+                            "Vdlaud",
+                            "GreenToxic",
+                            "Lewandowski",
+                            "Goretska",
+                            "Nouer",
+                            "Kimmich"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        title = "Contacts"
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me !")
+        print(footballPlayers[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return footballPlayers.count
+    }
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        //key of dic (massiv)
+//        <#code#>
+//    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = footballPlayers[indexPath.row]
+        
+        return cell
+    }
 }
 
